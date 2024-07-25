097c599acca523ed849c042dc0a09f34725eb8d0
Élaff Ramos <valelaff10@gmail.com>
2024-07-25 16:08:34 -0300

    Create variaveis.py

---
+++ variaveis.py
@@ -0,0 +1,5 @@
+musicas = ['another brick in the wall', 'stairway to heaven', 'We Will Rock You', 'My Generation']
+horarios = ['19:00', '19:45', '20:10', '20:30']
+
+for m, h in zip(musicas, horarios):
+    print(m,h)
